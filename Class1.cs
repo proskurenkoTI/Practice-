d079cb74170983488455da3f39963e2eaba5f565
tim <tim.r23@yandex.ru>
2025-03-11 01:16:25 +1000

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YourNamespace
+{
+    class NewProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from NewProgram!");
+        }
+    }
+}
